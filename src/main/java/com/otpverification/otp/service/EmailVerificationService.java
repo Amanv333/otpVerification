8da1d3021ec1fdb5688accfc29186e4f7428b2dd
Aman <soniamandav@gmail.com>
2024-02-26 11:40:47 +0530

    develope login feature via otp

--- src/main/java/com/otpverification/otp/service/EmailVerificationService.java
+++ src/main/java/com/otpverification/otp/service/EmailVerificationService.java
@@ -11,6 +11,8 @@ import java.util.Map;
 public class EmailVerificationService {
     @Autowired
     private UserService userService;
+    @Autowired
+    private EmailService emailService;
     static final Map<String,String> emailOtpMapping = new HashMap<>();
 
     public Map<String,String> verifyOtp(String email,String otp) {
@@ -37,4 +39,45 @@ public class EmailVerificationService {
     }
 
 
+    public Map<String, String> sendOtpForLogin(String email) {
+        if(userService.isEmailVerified(email)){
+            String storeOtp = emailService.OtpGenerator();
+            emailOtpMapping.put(email,storeOtp);
+
+            emailService.sendOtp(email);
+
+            Map<String,String> response = new HashMap<>();
+            response.put("status","success");
+            response.put("message","Otp sent successfully");
+            return response;
+
+        }
+        else {
+            Map<String,String> response = new HashMap<>();
+            response.put("status","error");
+            response.put("message","email is not verified");
+            return response;
+        }
+
+    }
+
+    public Map<String, String> verifyOtpForLogin(String email, String otp) {
+        String storeOtp = emailOtpMapping.get(email);
+
+        Map<String,String> response = new HashMap<>();
+        if (storeOtp!=null && storeOtp.equals(otp)){
+            //valid otp
+            response.put("status","success");
+            response.put("message","otp verified successfully");
+            return response;
+        }
+        else {
+            //invalid Otp
+            response.put("status","error");
+            response.put("message","Invalid Otp");
+            return response;
+        }
+
+
+    }
 }
